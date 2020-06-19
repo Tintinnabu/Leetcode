59d7a0f4c62112e879031bf012228dd9fc666ef5
Tinn_Desktop <545391738@qq.com>
2020-06-19 08:15:52 +0800

    update from laptop

--- src/main/java/top/tinn/DoublePointsProblem/Problem_125/Solution.java
+++ src/main/java/top/tinn/DoublePointsProblem/Problem_125/Solution.java
@@ -5,13 +5,13 @@ import org.junit.jupiter.api.Test;
 public class Solution {
     public boolean isPalindrome(String s) {
         int l=0,r=s.length()-1;
-        char[] tmp=s.toLowerCase().toCharArray();
+        char[] tmp=s.toCharArray();
         while (l<r){
             while (l<r&&!Character.isLetter(tmp[l])&&!Character.isDigit(tmp[l]))
                 l++;
             while (l<r&&!Character.isLetter(tmp[r])&&!Character.isDigit(tmp[r]))
                 r--;
-            if (tmp[l]==tmp[r]){
+            if ((tmp[l] + 32 - 'a') % 32 == (tmp[r] + 32 - 'a') % 32){
                 l++;r--;
             }else
                 return false;
